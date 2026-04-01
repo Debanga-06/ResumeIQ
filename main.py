e726b80d905f442c5ec181d08fdc75a869b49e51
DEBANGA <debanga078@gmail.com>
2026-04-01 11:31:04 +0530

    Change CORS settings to allow all origins

--- main.py
+++ main.py
@@ -106,11 +106,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://resumeiq-lite.onrender.com",
-        "http://localhost:3000",
-        "http://localhost:5173"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,  
     allow_methods=["*"],
     allow_headers=["*"],
